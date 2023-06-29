d35ea992bd6a7a42d2837ff869334b47d08ec6d2
Joseph Kossi A <42009253+jkossi@users.noreply.github.com>
2023-06-29 10:03:24 +0000

    Add schedulers report template (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import DailyGasOperations from "./pages/reports/DailyGasOperations";
 import DailyGasOfftakeRequest from "./pages/reports/DailyGasOfftakeRequest";
 import MonthlyDvcDeclaration from "./pages/reports/MonthlyDvcDeclaration";
 import MonthlyPnv from "./pages/reports/MonthlyPnv";
+import GasSchedulersLog from "~/pages/reports/GasSchedulersLog";
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         <Route path={routes.UI.DAILY_GAS_OFFTAKE_REQUEST()} element={<DailyGasOfftakeRequest />} />
         <Route path={routes.UI.MONTHLY_DVC_DECLARATION()} element={<MonthlyDvcDeclaration />} />
         <Route path={routes.UI.MONTHLY_PNV()} element={<MonthlyPnv />} />
+        <Route path={routes.UI.GAS_SCHEDULERS_LOG()} element={<GasSchedulersLog />} />
       </Route>
     </Routes>
   );
--- src/utils/constants/routes.js
+++ src/utils/constants/routes.js
@@ -4,6 +4,7 @@ const routes = {
     DAILY_GAS_NETWORK_EVENT_LOG: () => "/",
     DAILY_GAS_OFFTAKE_REQUEST: () => "/reports/daily_gas_offtake_request",
     DAILY_GAS_OPERATIONS: () => "/reports/daily_gas_operations",
+    GAS_SCHEDULERS_LOG: () => "/reports/gas_schedulers_log",
     MONTHLY_DVC_DECLARATION: () => "/reports/monthly_dvc_declaration",
     MONTHLY_PNV: () => "/reports/monthly_pnv",
   },
